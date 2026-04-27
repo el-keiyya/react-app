3f3211addee9bf171cefc4ac49ee0c50302221ee
el-keiyya <keiyya@outlook.com>
2026-04-27 13:29:53 +0300

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import "./App.css";
 
-function Greeting() {
-  return <h1>Hello, I am Haasan!</h1>;
+function Greeting(props) {
+  return (
+    <h1>
+      Hello, I am {props.name}, I am {props.age} years old!
+    </h1>
+  );
 }
 
 function App() {
-  const name = getName();
+  const name = "Hassan";
   const age = 29;
 
   return (
     <>
       <div>
-        <Greeting />
+        <Greeting name={"Hassan"} age={29} />
+        <Greeting name={"Pedro"} />
+        <Greeting name={"Jack"} />
+        <Greeting name={"Jessica"} />
+        <Greeting name={"Bane"} />
       </div>
     </>
   );
-}
-
-function getName() {
-  return "Hassan";
 }
 
 //COMPONENT = Function that returns JSX
