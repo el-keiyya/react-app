9fcea1eac6d5c2122f9eace424910ab75d7b562f
el-keiyya <keiyya@outlook.com>
2026-04-27 13:22:14 +0300

    Calling variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import "./App.css";
 
 function App() {
+  const name = getName();
+  const age = 29;
+
   return (
     <>
       <div>
-        <h1>Hello World</h1>
+        <h1>
+          Hello World, I am {name} and I'm {age} years old
+        </h1>
       </div>
     </>
   );
+}
+
+function getName() {
+  return "Hassan";
 }
 
 export default App;
