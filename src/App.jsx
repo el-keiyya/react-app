747fdb8b82b648dc9452725180e6d6d1ee9de4a5
el-keiyya <keiyya@outlook.com>
2026-05-05 16:48:42 +0300

    SignUpForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import { useState } from "react";
 import "./App.css";
-
-function TodoList() {
-  const todos = [
-    { id: 1, text: "Learn JSX" },
-    { id: 2, text: "Understand props" },
-    { id: 3, text: "Master state & events" },
-  ];
-
-  return (
-    <div>
-      <h2>My React Todos</h2>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.text}</li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
   return (
     <div>
-      <TodoList />
+      <SignUpForm />
     </div>
   );
 }
